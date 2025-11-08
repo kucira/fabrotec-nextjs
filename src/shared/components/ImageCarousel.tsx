6b04463ebf7a1781a26b86c8d8e09e2ca354640b
kucira <kucira@users.noreply.github.com>
2025-11-08 08:01:03 +0700

    Update ImageCarousel.tsx

--- src/shared/components/ImageCarousel.tsx
+++ src/shared/components/ImageCarousel.tsx
@@ -1,45 +1,10 @@
 const ImageCarousel = ({ images }: { images: string[] }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handlePrevious = () => {
-    setCurrentIndex(currentIndex - 1);
-  };
-
-  const handleNext = () => {
-    setCurrentIndex(currentIndex + 1);
-  };
-
+ 
   return (
-    <div className="relative">
-      <div className="flex items-center justify-center">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            className={
-              currentIndex === index
-                ? "block w-full"
-                : "hidden"
-            }
-            alt=""
-          />
-        ))}
-      </div>
-      <button
-        className="absolute top-1/2 left-0 transform -translate-y-1/2 p-2"
-        onClick={handlePrevious}
-      >
-        {"\u25c0"}
-      </button>
-      <button
-        className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2"
-        onClick={handleNext}
-      >
-        {"\u25b6"}
-      </button>
-    </div>
+   <div>hello</div>
   );
 };
 
 export default ImageCarousel;
+
 
